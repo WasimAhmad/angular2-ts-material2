b247269e84844023979bf2b36bd66ad42f759053
janiclassical <janiclassical@gmail.com>
2016-06-25 04:19:19 +0500

    button is not working

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import {MdButton} from '@angular2-material/button';
+
 
 @Component({
   selector: 'my-app',
-  templateUrl : 'app/templates/home.html'
+  templateUrl : 'app/templates/home.html',
+  directives: [MdButton]
 })
 
 export class AppComponent {
 
- }
+}
