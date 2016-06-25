ba37a4ad8a7e4caabc5b3a65128f24a39ab9dba9
janiclassical <janiclassical@gmail.com>
2016-06-25 11:57:38 +0500

    .gitignnore updated

--- systemjs.config.js
+++ systemjs.config.js
@@ -5,6 +5,13 @@
 (function (global) {
     // map tells the System loader where to look for things
     var map = {
+        '@angular2-material/core': 'node_modules/@angular2-material/core',
+        '@angular2-material/checkbox': 'node_modules/@angular2-material/checkbox',
+        '@angular2-material/button': 'node_modules/@angular2-material/button',
+        '@angular2-material/progress-circle': 'node_modules/@angular2-material/progress-circle',
+        '@angular2-material/card': 'node_modules/@angular2-material/card',
+        '@angular2-material/input': 'node_modules/@angular2-material/input',
+        '@angular2-material/toolbar': 'node_modules/@angular2-material/toolbar',
         'app': 'app',
         '@angular': 'node_modules/@angular',
         'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
@@ -12,6 +19,13 @@
     };
     // packages tells the System loader how to load when no filename and/or no extension
     var packages = {
+        '@angular2-material/core': { main: 'core.js', defaultExtension: 'js' },
+        '@angular2-material/checkbox': { main: 'checkbox.js', defaultExtension: 'js' },
+        '@angular2-material/button': { main: 'button.js', defaultExtension: 'js' },
+        '@angular2-material/progress-circle': { main: 'progress-circle.js', defaultExtension: 'js' },
+        '@angular2-material/card': { main: 'card.js', defaultExtension: 'js' },
+        '@angular2-material/input': { main: 'input.js', defaultExtension: 'js' },
+        '@angular2-material/toolbar': { main: 'toolbar.js', defaultExtension: 'js' },
         'app': { main: 'main.js', defaultExtension: 'js' },
         'rxjs': { defaultExtension: 'js' },
         'angular2-in-memory-web-api': { main: 'index.js', defaultExtension: 'js' },
