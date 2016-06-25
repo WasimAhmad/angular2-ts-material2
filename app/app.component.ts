86a2ded80f3a131fc240c01f5f07d2598021ac10
janiclassical <janiclassical@gmail.com>
2016-06-25 13:54:50 +0500

    updated

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 import {MD_SIDENAV_DIRECTIVES} from '@angular2-material/sidenav';
 import {MD_LIST_DIRECTIVES} from '@angular2-material/list';
 import {MD_CARD_DIRECTIVES} from '@angular2-material/card';
@@ -7,22 +6,50 @@ import {MdButton} from '@angular2-material/button';
 import {MdToolbar} from '@angular2-material/toolbar';
 import {MdIcon,MdIconRegistry} from '@angular2-material/icon';
 import {MD_INPUT_DIRECTIVES} from '@angular2-material/input';
+import {MdCheckbox} from '@angular2-material/checkbox';
+import {MdRadioButton, MdRadioGroup, MdRadioDispatcher} from '@angular2-material/radio';
 
 
 
 @Component({
   selector: 'my-app',
-  templateUrl : 'app/templates/home.html',
+  templateUrl : 'app/app.dashboard.html',
   directives: [
     MD_SIDENAV_DIRECTIVES,
     MD_LIST_DIRECTIVES,
     MD_CARD_DIRECTIVES,
     MdButton,
     MdToolbar,
-    MD_INPUT_DIRECTIVES
-      ]
+    MD_INPUT_DIRECTIVES,
+    MdCheckbox,
+    MdCheckbox,
+    MdRadioGroup,
+    MdRadioButton,
+    MdIcon
+      ],
+    providers: [MdIconRegistry, MdRadioDispatcher],
 })
 
 export class AppComponent {
-
+ formShowing: boolean = false;
+  views: Object[] = [
+    {
+      name: "My Account",
+      description: "Edit my account information",
+      icon: "assignment ind"
+    },
+    {
+      name: "Potential dates",
+      description: "Find your soulmate!",
+      icon: "pets"
+    }
+  ];
+  dogs: Object[] = [
+    {name: "Porter"},
+    {name: "Mal"},
+    {name: "Razzle"},
+    {name: "Koby"},
+    {name: "Molly"},
+    {name: "Husi"}
+  ];
 }
--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,11 @@
 import { bootstrap }    from '@angular/platform-browser-dynamic';
 import { AppComponent } from './app.component';
+import { environment} from './app.environment';
+import {enableProdMode} from '@angular/core';
+import {HTTP_PROVIDERS} from '@angular/http';
 
+if (environment.production) {
+  enableProdMode();
+}
 
-
-bootstrap(AppComponent);
+bootstrap(AppComponent, [HTTP_PROVIDERS]);
